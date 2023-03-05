c31254379e200832150ebb894646d24a547eaff1
Stephanie Martinez Muñoz <caicedo.edg@gmail.com>
2023-03-04 21:47:42 -0500

    modificamos una clase con una variable

--- src/main/java/com/pruebaNeoris/pruebaNeoris/PruebaNeorisApplication.java
+++ src/main/java/com/pruebaNeoris/pruebaNeoris/PruebaNeorisApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PruebaNeorisApplication {
+	
+	String prueba = "";
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaNeorisApplication.class, args);
